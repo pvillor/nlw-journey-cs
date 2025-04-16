1d4d66e24919543710e6a9f73f5e895b1104c166
paulo.guerra <paulo.guerra@flex-am.com.br>
2025-04-16 16:40:42 -0400

    first class

---
+++ src/Journey.Infrastructure/JourneyDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Journey.Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Journey.Infrastructure;
+
+public class JourneyDbContext : DbContext
+{
+    public DbSet<Trip> Trips { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite(@"Data Source=C:\Users\paulo.guerra\Documents\rocketseat\nlw-journey\JourneyDatabase.db");
+    }
+}
